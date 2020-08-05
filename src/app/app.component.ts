e422b61ff2d7f60f7303ab296e2ea41422a19c9b
tamikaivi <yukisaki1998@gmail.com>
2020-08-05 15:57:37 -0400

    observer 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {from, fromEvent } from 'rxjs'
+import {from, fromEvent } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
